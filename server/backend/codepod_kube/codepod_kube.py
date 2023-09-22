eff97075d76c76b32f42eac4fb424936b9cc6a35
annleefores <annleefores@gmail.com>
2023-09-22 19:13:26 +0530

    backend codepod creation funciton

--- server/backend/codepod_kube/codepod_kube.py
+++ server/backend/codepod_kube/codepod_kube.py
@@ -1,14 +1,16 @@
+from typing import Dict
 from kubernetes import client, config, utils
 import os
 
+if os.getenv("BACKEND_ENV") == "kube":
+    config.load_kube_config()
+else:
+    config.load_incluster_config()
 
-def main():
-    if os.getenv("BACKEND_DEV_ENV"):
-        config.load_kube_config()
-    else:
-        config.load_incluster_config()  # From a pod within the cluster
+k8s_client = client.ApiClient()
 
-    k8s_client = client.ApiClient()
+
+def create() -> Dict[str, str]:
     codepod_yaml_file = "manifest/codepod.yml"
     utils.create_from_yaml(k8s_client, codepod_yaml_file, verbose=True)
     codepod_ingress_yaml_file = "manifest/codepod-ingress.yml"
--- server/backend/main.py
+++ server/backend/main.py
@@ -1,18 +1,31 @@
 from typing import Dict
-import uvicorn
-from fastapi import FastAPI, status
-from codepod_kube.codepod_kube import main
+from fastapi import FastAPI, HTTPException, status
 from dotenv import load_dotenv
+import uvicorn
 
 load_dotenv()
+
+from codepod_kube.codepod_kube import create
+
 
 app = FastAPI()
 
 
-@app.get("/", status_code=status.HTTP_201_CREATED)
-def read_root() -> Dict[str, str]:
-    main()
+@app.get("/create", status_code=status.HTTP_201_CREATED)
+def create_codepod() -> Dict[str, str]:
+    try:
+        create()
+    except Exception as err:
+        print(err)
+        raise HTTPException(status_code=500, detail="Codepod creation failed")
+
     return {"success": "codepod created successfully"}
+
+
+@app.get("/delete", status_code=status.HTTP_201_CREATED)
+def delete_codepod() -> Dict[str, str]:
+    # create()
+    return {"success": "codepod deleted successfully"}
 
 
 if __name__ == "__main__":
