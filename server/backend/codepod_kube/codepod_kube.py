0834458e3e039150d336d3fea505e5998b62e2a2
annleefores <annleefores@gmail.com>
2023-09-23 15:28:06 +0530

    configure codepod using k8s python models

--- server/backend/codepod_kube/codepod_kube.py
+++ server/backend/codepod_kube/codepod_kube.py
@@ -1,5 +1,6 @@
-from typing import Dict
-from kubernetes import client, config, utils
+from typing import Any
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 import os
 
 if os.getenv("BACKEND_ENV") == "kube":
@@ -7,11 +8,190 @@ if os.getenv("BACKEND_ENV") == "kube":
 else:
     config.load_incluster_config()
 
-k8s_client = client.ApiClient()
+
+def create_pod(name: str):
+    v1 = client.CoreV1Api()
+
+    if read_resources(v1, name, "pod"):
+        print(f"{name} pod already exists")
+        return 1
+
+    print(f"Creating {name} pod...")
+
+    pod_spec = client.V1PodSpec(
+        containers=[
+            client.V1Container(
+                name="codepod",
+                image="annleefores/codepod-prod:1.0.0",
+                ports=[client.V1ContainerPort(container_port=5000)],
+                image_pull_policy="IfNotPresent",
+                resources=client.V1ResourceRequirements(
+                    limits={"memory": "500Mi", "cpu": "1000m"}
+                ),
+            )
+        ]
+    )
+    pod_metadata = client.V1ObjectMeta(name=name, labels={"app": name})
+    pod_body = client.V1Pod(
+        api_version="v1", kind="Pod", metadata=pod_metadata, spec=pod_spec
+    )
+
+    resp = v1.create_namespaced_pod(namespace="default", body=pod_body)
+
+    return resp
 
 
-def create() -> Dict[str, str]:
-    codepod_yaml_file = "manifest/codepod.yml"
-    utils.create_from_yaml(k8s_client, codepod_yaml_file, verbose=True)
-    codepod_ingress_yaml_file = "manifest/codepod-ingress.yml"
-    utils.create_from_yaml(k8s_client, codepod_ingress_yaml_file, verbose=True)
+def delete_pod(name: str):
+    v1 = client.CoreV1Api()
+
+    if read_resources(v1, name, "pod") is None:
+        print(f"{name} pod does not exist or already deleted....")
+        return 1
+
+    print(f"Deleting {name} pod....")
+
+    del_resp = None
+
+    try:
+        del_resp = v1.delete_namespaced_pod(namespace="default", name=name)
+    except ApiException as e:
+        raise Exception
+
+    return del_resp
+
+
+def create_svc(name: str):
+    v1 = client.CoreV1Api()
+
+    if read_resources(v1, name, "service"):
+        print(f"{name} service already exists")
+        return 1
+
+    print(f"Creating {name} svc....")
+
+    svc_metadata = client.V1ObjectMeta(name=name, labels={"app": name})
+
+    svc_spec = client.V1ServiceSpec(
+        selector={"app": name},
+        type="ClusterIP",
+        ports=[client.V1ServicePort(port=5000, target_port=5000)],
+    )
+
+    svc_body = client.V1Service(
+        api_version="v1", kind="Service", metadata=svc_metadata, spec=svc_spec
+    )
+
+    resp = v1.create_namespaced_service(namespace="default", body=svc_body)
+
+    return resp
+
+
+def delete_svc(name: str):
+    v1 = client.CoreV1Api()
+
+    if read_resources(v1, name, "service") is None:
+        print(f"{name} service does not exist or already deleted....")
+        return 1
+
+    print(f"Deleting {name} svc....")
+
+    del_resp = None
+    try:
+        del_resp = v1.delete_namespaced_service(namespace="default", name=name)
+    except ApiException as e:
+        raise Exception
+
+    return del_resp
+
+
+def create_ingress(name: str):
+    v1 = client.NetworkingV1Api()
+
+    if read_resources(v1, name, "ingress"):
+        print(f"{name} ingress already exists")
+        return 1
+
+    print(f"Creating {name} ingress....")
+
+    ingress_metadata = client.V1ObjectMeta(
+        name=name,
+        labels={"app": name},
+        annotations={
+            "nginx.ingress.kubernetes.io/proxy-read-timeout": "3600",
+            "nginx.ingress.kubernetes.io/proxy-send-timeout": "3600",
+        },
+    )
+
+    ingress_spec = client.V1IngressSpec(
+        ingress_class_name="nginx",
+        rules=[
+            client.V1IngressRule(
+                http=client.V1HTTPIngressRuleValue(
+                    paths=[
+                        client.V1HTTPIngressPath(
+                            path_type="Prefix",
+                            path="/ws/",
+                            backend=client.V1IngressBackend(
+                                service=client.V1IngressServiceBackend(
+                                    name=name,
+                                    port=client.V1ServiceBackendPort(
+                                        number=5000,
+                                    ),
+                                )
+                            ),
+                        )
+                    ]
+                )
+            )
+        ],
+    )
+
+    ingress_body = client.V1Service(
+        api_version="networking.k8s.io/v1",
+        kind="Ingress",
+        metadata=ingress_metadata,
+        spec=ingress_spec,
+    )
+
+    resp = v1.create_namespaced_ingress(namespace="default", body=ingress_body)
+
+    return resp
+
+
+def delete_ingress(name: str):
+    v1 = client.NetworkingV1Api()
+
+    if read_resources(v1, name, "ingress") is None:
+        print(f"{name} ingress does not exist or already deleted....")
+        return 1
+
+    print(f"Deleting {name} ingress....")
+
+    del_resp = None
+
+    try:
+        del_resp = v1.delete_namespaced_ingress(namespace="default", name=name)
+    except ApiException as e:
+        raise Exception
+
+    return del_resp
+
+
+def read_resources(
+    v1: Any,
+    name: str,
+    resource: str,
+    namespace: str = "default",
+) -> bool:
+    resp = None
+
+    try:
+        method_name = f"read_namespaced_{resource}"
+        get_method = getattr(v1, method_name)
+        resp = get_method(name=name, namespace=namespace)
+    except ApiException as e:
+        if e.status != 404:
+            print(f"Unknown error: {e}")
+            exit(1)
+    # return None or some response
+    return resp
--- server/backend/main.py
+++ server/backend/main.py
@@ -1,11 +1,20 @@
 from typing import Dict
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, status
 from dotenv import load_dotenv
 import uvicorn
+from kubernetes.client.rest import ApiException
+import logging
 
 load_dotenv()
 
-from codepod_kube.codepod_kube import create
+from codepod_kube.codepod_kube import (
+    create_ingress,
+    create_pod,
+    create_svc,
+    delete_ingress,
+    delete_pod,
+    delete_svc,
+)
 
 
 app = FastAPI()
@@ -13,18 +22,84 @@ app = FastAPI()
 
 @app.get("/create", status_code=status.HTTP_201_CREATED)
 def create_codepod() -> Dict[str, str]:
-    try:
-        create()
-    except Exception as err:
-        print(err)
-        raise HTTPException(status_code=500, detail="Codepod creation failed")
+    # add randomness to pod name
+    name = "codepod"
 
-    return {"success": "codepod created successfully"}
+    exceptions = {
+        "create_pod": False,
+        "create_svc": False,
+        "create_ingress": False,
+    }
+
+    # create pod
+    try:
+        created_pod_resp = create_pod(name=name)
+    except ApiException as e:
+        logging.exception("Exception when calling CoreV1Api->create_namespaced_pod:")
+        exceptions["create_pod"] = True
+
+    # create svc
+    try:
+        created_svc_resp = create_svc(name=name)
+    except ApiException as e:
+        logging.exception(
+            "Exception when calling CoreV1Api->create_namespaced_service:"
+        )
+        exceptions["create_svc"] = True
+
+    # create ingress
+    try:
+        created_ingress_resp = create_ingress(name=name)
+    except ApiException as e:
+        logging.exception(
+            "Exception when calling NetworkingV1Api->create_namespaced_ingress:",
+        )
+        exceptions["create_ingress"] = True
+
+    for func, stat in exceptions.items():
+        if stat:
+            print(f"{func} caused an error")
+
+    return {"success": "codepod created successfully", "pod_name": name}
 
 
 @app.get("/delete", status_code=status.HTTP_201_CREATED)
 def delete_codepod() -> Dict[str, str]:
-    # create()
+    # get name from client
+    name = "codepod"
+
+    exceptions = {
+        "delete_pod": False,
+        "delete_svc": False,
+        "delete_ingress": False,
+    }
+
+    # delete pod
+    try:
+        deleted_pod_resp = delete_pod(name=name)
+    except ApiException as e:
+        logging.exception("Exception when calling CoreV1Api->delete_namespaced_pod")
+        exceptions["delete_pod"] = True
+
+    # delete svc
+    try:
+        deleted_svc_resp = delete_svc(name=name)
+    except ApiException as e:
+        logging.exception("Exception when calling CoreV1Api->delete_namespaced_service")
+        exceptions["delete_svc"] = True
+
+    # delete ingress
+    try:
+        deleted_ingress_resp = delete_ingress(name=name)
+    except ApiException as e:
+        logging.exception("Exception when calling CoreV1Api->delete_namespaced_service")
+
+        exceptions["delete_ingress"] = True
+
+    for func, stat in exceptions.items():
+        if stat:
+            print(f"{func} caused an error")
+
     return {"success": "codepod deleted successfully"}
 
 
