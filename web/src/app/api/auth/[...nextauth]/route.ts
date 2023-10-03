12ff0b45c2d442473fa3733e726e81fc367d3246
annleefores <annleefores@gmail.com>
2023-10-03 22:23:54 +0530

    configure nextauth

---
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import type { NextAuthOptions } from "next-auth";
+import { z } from "zod";
+
+const envSchema = z.object({
+  GITHUB_ID: z.string().nonempty(),
+  GITHUB_SECRET: z.string().nonempty(),
+});
+
+const result = envSchema.safeParse(process.env);
+
+if (!result.success) {
+  console.error(result.error);
+  throw new Error("Invalid environment variables");
+}
+
+const config = result.data;
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: config.GITHUB_ID,
+      clientSecret: config.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
