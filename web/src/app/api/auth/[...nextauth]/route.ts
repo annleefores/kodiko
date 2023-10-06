2fb8c269262e23e3499c0197d9a3bf33f4774271
annleefores <annleefores@gmail.com>
2023-10-06 21:00:42 +0530

    remove nextauth callback

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -26,21 +26,6 @@ const authOptions: NextAuthOptions = {
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET,
-
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken as string;
-      return session;
-    },
-  },
 };
 
 const handler = NextAuth(authOptions);
