1443eb2a56b6edc93e0fae58f7d421a5418902e3
annleefores <annleefores@gmail.com>
2023-10-12 19:26:27 +0530

    added websocket server auth

--- server/codepod/src/wsService.ts
+++ server/codepod/src/wsService.ts
@@ -1,5 +1,27 @@
 import WebSocket from "ws";
 import { Pty } from "./terminal";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
+
+// Verifier that expects valid access tokens:
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: process.env.COGNITO_USER_POOL_ID!,
+  tokenUse: "access",
+  clientId: process.env.COGNITO_CLIENT_ID!,
+});
+
+const verify = async (token: string) => {
+  try {
+    const payload = await verifier.verify(token);
+    return payload.username;
+  } catch (error) {
+    console.log("Token not valid!", error);
+    return null;
+  }
+};
 
 export class wsService {
   private ws: WebSocket | null;
@@ -24,34 +46,41 @@ export class wsService {
     wss.on("connection", (ws: WebSocket, request) => {
       console.log("Client connected to socket");
 
-      console.log(request);
+      const accessToken = request.headers["sec-websocket-protocol"]!;
 
-      this.ws = ws;
-
-      this.ws.on("error", console.error);
-
-      this.ws.on("close", () => {
-        console.log("Client disconnected");
-      });
-
-      this.pty = new Pty(this.ws);
-
-      this.ws.on("message", (input: any) => {
-        // convert buffer to string
-        const inputString = input.toString();
-
-        // TODO: fix CTRL+D error
-
-        // console.log("input-string", inputString);
-
-        if (inputString.startsWith("\x04")) {
-          // Remove ASCII character and convert string to JSON
-          const jsonData = JSON.parse(inputString.replace(/\x04/g, ""));
-          // console.log(jsonData);
-          this.pty?.reSize(jsonData.cols, jsonData.rows);
-        } else {
-          this.pty?.write(inputString);
+      verify(accessToken).then((resp) => {
+        if (resp === null) {
+          ws.send("HTTP/1.1 401 Unauthorized\r\n\r\n");
+          ws.close();
         }
+
+        this.ws = ws;
+
+        this.ws.on("error", console.error);
+
+        this.ws.on("close", () => {
+          console.log("Client disconnected");
+        });
+
+        this.pty = new Pty(this.ws);
+
+        this.ws.on("message", (input: any) => {
+          // convert buffer to string
+          const inputString = input.toString();
+
+          // TODO: fix CTRL+D error
+
+          // console.log("input-string", inputString);
+
+          if (inputString.startsWith("\x04")) {
+            // Remove ASCII character and convert string to JSON
+            const jsonData = JSON.parse(inputString.replace(/\x04/g, ""));
+            // console.log(jsonData);
+            this.pty?.reSize(jsonData.cols, jsonData.rows);
+          } else {
+            this.pty?.write(inputString);
+          }
+        });
       });
     });
   }
