ac8974e2b1a1740459792f98208d61f387a01590
annleefores <annleefores@gmail.com>
2023-10-18 22:44:11 +0530

    tf init

---
+++ infra/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+
+
+  backend "s3" {
+    bucket         = "annlee-s3-state"
+    key            = "kodiko/kodiko.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-db"
+    encrypt        = true
+  }
+
+  required_version = "~> 1.6.1"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source = "./vpc"
+}
---
+++ infra/vpc/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "annlee-s3-state"
+    key            = "kodiko/kodiko_vpc.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-db"
+    encrypt        = true
+  }
+
+  required_version = "~> 1.6.1"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "kodiko_vpc" {
+  cidr_block = var.vpc_cidr_bloc
+
+  tags = {
+    Name = "kodiko"
+  }
+}
