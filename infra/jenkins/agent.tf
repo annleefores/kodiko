daada8e1cde8ed98fb24dea9d6450898dd1c3c71
annleefores <annleefores@gmail.com>
2023-11-09 22:58:07 +0530

    split jenkins iac to different files

--- infra/jenkins/agent.tf
+++ infra/jenkins/agent.tf
@@ -1,31 +1,17 @@
-data "aws_ami" "ami_id" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = [var.ami_image_name]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = var.ami_owners
-}
 
 data "local_file" "docker_config" {
   filename = "${path.module}/docker_config.sh"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "agent" {
   ami                         = data.aws_ami.ami_id.id
   instance_type               = var.instance_type
   key_name                    = var.key_name
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   user_data                   = data.local_file.docker_config.content
-  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
+  vpc_security_group_ids      = [aws_security_group.jenkins_agent_sg.id]
 
 
   credit_specification {
@@ -33,11 +19,11 @@ resource "aws_instance" "web" {
   }
 
   tags = {
-    Name = "kodiko_jenkins"
+    Name = "kodiko_jenkins_agent"
   }
 }
 
-resource "aws_security_group" "jenkins_sg" {
+resource "aws_security_group" "jenkins_agent_sg" {
   name        = "jenkins_sg"
   description = "Allow SSH and HTTP"
   vpc_id      = var.vpc_id
@@ -47,20 +33,20 @@ resource "aws_security_group" "jenkins_sg" {
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "jenkins_sg_egress_1" {
-  security_group_id = aws_security_group.jenkins_sg.id
+resource "aws_vpc_security_group_egress_rule" "jenkins_agent_sg_egress_1" {
+  security_group_id = aws_security_group.jenkins_agent_sg.id
 
   description = "all_outbound"
   cidr_ipv4   = "0.0.0.0/0"
   ip_protocol = "-1"
 
   tags = {
-    Name = "jenkins_sg_egress_1"
+    Name = "jenkins_agent_sg_egress_1"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "jenkins_sg_ingress_1" {
-  security_group_id = aws_security_group.jenkins_sg.id
+resource "aws_vpc_security_group_ingress_rule" "jenkins_agent_sg_ingress_1" {
+  security_group_id = aws_security_group.jenkins_agent_sg.id
 
   description = "Jenkins"
   cidr_ipv4   = "0.0.0.0/0"
@@ -69,12 +55,12 @@ resource "aws_vpc_security_group_ingress_rule" "jenkins_sg_ingress_1" {
   to_port     = 8080
 
   tags = {
-    Name = "jenkins_sg_ingress_1"
+    Name = "jenkins_agent_sg_ingress_1"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "jenkins_sg_ingress_2" {
-  security_group_id = aws_security_group.jenkins_sg.id
+resource "aws_vpc_security_group_ingress_rule" "jenkins_agent_sg_ingress_2" {
+  security_group_id = aws_security_group.jenkins_agent_sg.id
 
   description = "SSH"
   cidr_ipv4   = "0.0.0.0/0"
@@ -83,7 +69,7 @@ resource "aws_vpc_security_group_ingress_rule" "jenkins_sg_ingress_2" {
   to_port     = 22
 
   tags = {
-    Name = "jenkins_sg_ingress_2"
+    Name = "jenkins_agent_sg_ingress_2"
   }
 }
 
